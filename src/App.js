050ddc3248a27129548431b8429f2d3ee5da3765
Alok3333 <alokprajapati697@gmail.com>
2024-12-30 21:14:23 +0530

    process...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const dictionaryState = [
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." },
+];
 
 function App() {
+  const [searchTxt, setSearchTxt] = useState("");
+  const [showDictionaryText, setShowDictionaryText] = useState([]);
+
+  const handleClickSearch = (e) => {
+    e.preventDefault();
+
+    let filterState = dictionaryState.filter((w) => w.word.toLocaleLowerCase() === searchTxt.toLocaleLowerCase());
+    // console.log(filterState, "here");
+    setShowDictionaryText(filterState);
+  }
+
   return (
     <div className="App">
-      xDictionary
+      <div>
+        <h1>Dictionary App</h1>
+      </div>
+      <div>
+        <form>
+          <input type="text" value={searchTxt} placeholder="Search for a word..." onChange={(e) => setSearchTxt(e.target.value)}/>
+          <button onClick={handleClickSearch}>Search</button>
+        </form>
+      </div>
+      <div>
+        <p>
+          <b>Defination:</b>
+        </p>
+        {showDictionaryText.map((item) => (
+          <p key={item.word}>{item.meaning}</p>
+        ))}
+      </div>
     </div>
   );
 }
